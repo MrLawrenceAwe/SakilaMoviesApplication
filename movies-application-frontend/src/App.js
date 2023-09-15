176682d6d341ad5d4b71e0d36307bda54ca7be95
MrLawrenceAwe <mrlawrenceawe@gmail.com>
2023-09-15 11:11:14 +0100

    Refactored FilmList.js to only be a list of films and put everything else into App.js

--- movies-application-frontend/src/App.js
+++ movies-application-frontend/src/App.js
@@ -5,10 +5,26 @@ import AddFilmForm from './AddFilmForm';
 import { filmAPIClient } from './APIClients/filmAPIClient';
 import CollapsibleSection from './CollapsibleSection';
 import AddActorForm from './AddActorForm';
+import { Button, TextField, Typography, Box } from '@mui/material';
 
 
 function App() {
+  const [films, setFilms] = useState([]);
+  const [title, setTitle] = useState('');
   const [error, setError] = useState(null);
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  }
+
+  const searchFilm = () => {
+      filmAPIClient.getFilmByTitle(title)
+        .then(film => {
+            setFilms([film]);  
+              setError(null);
+        })
+        .catch(error => setError(error.message));
+    }
 
   const handleFilmSubmit = (newFilm) => {
     filmAPIClient.createFilm(newFilm)
@@ -22,7 +38,25 @@ function App() {
     <div className="App">
       <h1>Sakila Movies</h1>
       <hr/>
-      <FilmList />
+      {/* Search Film By Title */}
+      <Box sx={{ marginBottom: 3 }}>
+          <Typography variant="h6">Search Film</Typography>
+          <TextField 
+              variant="outlined" 
+              fullWidth 
+              value={title} 
+              onChange={handleTitleChange} 
+              label="Search film by title"
+          />
+          <Button 
+              variant="contained" 
+              color="primary" 
+              onClick={searchFilm} 
+              sx={{ marginTop: 2 }}>
+              Search
+          </Button>
+      </Box>
+      <FilmList films={films} onSearch={searchFilm} />
       <AddFilmForm onFilmSubmit={handleFilmSubmit} />
       <hr />
       {/* Error Message */}
--- movies-application-frontend/src/FilmList.js
+++ movies-application-frontend/src/FilmList.js
@@ -1,46 +1,6 @@
-import React, { useState } from 'react';
-import { filmAPIClient } from './APIClients/filmAPIClient';
-import { Button, TextField, Typography, Box } from '@mui/material';
-
-const FilmList = () => {
-    const [films, setFilms] = useState([]);
-    const [title, setTitle] = useState('');
-    const [error, setError] = useState(null);
-
-    const handleTitleChange = (e) => {
-        setTitle(e.target.value);
-    }
-
-    const searchFilm = () => {
-        filmAPIClient.getFilmByTitle(title)
-            .then(film => {
-                setFilms([film]);  
-                setError(null);
-            })
-            .catch(error => setError(error.message));
-    }
-
+const FilmList = ({films}) => {
     return (
         <div>
-            {/* Search Film By Title */}
-            <Box sx={{ marginBottom: 3 }}>
-                <Typography variant="h6">Search Film</Typography>
-                <TextField 
-                    variant="outlined" 
-                    fullWidth 
-                    value={title} 
-                    onChange={handleTitleChange} 
-                    label="Search film by title"
-                />
-                <Button 
-                    variant="contained" 
-                    color="primary" 
-                    onClick={searchFilm} 
-                    sx={{ marginTop: 2 }}>
-                    Search
-                </Button>
-            </Box>
-
             {/* List of Films */}
             <div>
               <h2>Films</h2>
