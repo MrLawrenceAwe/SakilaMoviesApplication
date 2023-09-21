61ac8d81ee869e2244e18e051ac23dee7ad0c62e
MrLawrenceAwe <mrlawrenceawe@gmail.com>
2023-09-21 11:47:21 +0100

    Removed redundant fields from Film DTO

--- src/main/java/com/LawrenceAwe/MoviesApplication/Film.java
+++ src/main/java/com/LawrenceAwe/MoviesApplication/Film.java
@@ -2,10 +2,6 @@ package com.LawrenceAwe.MoviesApplication;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import java.math.BigDecimal;
-import java.sql.Timestamp;
-import java.util.Set;
-
 @JsonInclude(JsonInclude.Include.NON_NULL) // Ensures fields that are null won't be included
 public class Film {
 
@@ -20,24 +16,11 @@ public class Film {
     private String language;
     private String languageId;
 
-    private String originalLanguageId;
-
-    private Short rentalDuration;
-
-    private BigDecimal rentalRate;
-
     private String length;
-
-    private BigDecimal replacementCost;
 
     private String rating;
 
-    private Set<String> specialFeatures;
-
-    private Timestamp lastUpdate;
-
     private String category;
-    private String categoryId;
 
     // Default constructor for deserialization
     public Film() {}
@@ -48,30 +31,8 @@ public class Film {
         this.description = description;
         this.releaseYear = releaseYear;
         this.languageId = languageId;
-        this.originalLanguageId = originalLanguageId;
         this.length = length;
         this.rating = rating;
-    }
-
-    public Film(int filmId, String title, String description,
-                Integer releaseYear, String languageId, String originalLanguageId,
-                Short rentalDuration, BigDecimal rentalRate, String length,
-                BigDecimal replacementCost, String rating, Set<String> specialFeatures,
-                Timestamp lastUpdate) {
-
-        this.filmId = String.valueOf(filmId);
-        this.title = title;
-        this.description = description;
-        this.releaseYear = releaseYear;
-        this.languageId = languageId;
-        this.originalLanguageId = originalLanguageId;
-        this.rentalDuration = rentalDuration;
-        this.rentalRate = rentalRate;
-        this.length = length;
-        this.replacementCost = replacementCost;
-        this.rating = rating;
-        this.specialFeatures = specialFeatures;
-        this.lastUpdate = lastUpdate;
     }
 
     public String getFilmId() {
@@ -114,44 +75,12 @@ public class Film {
         this.languageId = languageId;
     }
 
-    public String getOriginalLanguageId() {
-        return originalLanguageId;
-    }
-
-    public void setOriginalLanguageId(String originalLanguageId) {
-        this.originalLanguageId = originalLanguageId;
-    }
-
-    public Short getRentalDuration() {
-        return rentalDuration;
-    }
-
-    public void setRentalDuration(Short rentalDuration) {
-        this.rentalDuration = rentalDuration;
-    }
-
-    public BigDecimal getRentalRate() {
-        return rentalRate;
-    }
-
-    public void setRentalRate(BigDecimal rentalRate) {
-        this.rentalRate = rentalRate;
-    }
-
     public String getLength() {
         return length;
     }
 
     public void setLength(String length) {
-        this.length = length;
-    }
-
-    public BigDecimal getReplacementCost() {
-        return replacementCost;
-    }
-
-    public void setReplacementCost(BigDecimal replacementCost) {
-        this.replacementCost = replacementCost;
+        this.length = length == null ? null : length.trim();
     }
 
     public String getRating() {
@@ -160,22 +89,6 @@ public class Film {
 
     public void setRating(String rating) {
         this.rating = rating == null ? null : rating.trim();
-    }
-
-    public Set<String> getSpecialFeatures() {
-        return specialFeatures;
-    }
-
-    public void setSpecialFeatures(Set<String> specialFeatures) {
-        this.specialFeatures = specialFeatures;
-    }
-
-    public Timestamp getLastUpdate() {
-        return lastUpdate;
-    }
-
-    public void setLastUpdate(Timestamp lastUpdate) {
-        this.lastUpdate = lastUpdate;
     }
 
     public String getLanguage() {
@@ -192,9 +105,5 @@ public class Film {
 
     public void setCategory(String category) {
     	this.category = category;
-    }
-
-    public String getCategoryId() {
-        return this.categoryId;
     }
 }
